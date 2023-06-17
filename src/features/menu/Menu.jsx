5d86b58f9b0ce5e5f7943e7ff7bfb1e34e060d3f
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-17 16:57:30 +1000

    Add MenuItem component

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,15 +1,21 @@
 import { useLoaderData } from 'react-router-dom';
 import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
 export default function Menu() {
 	const menu = useLoaderData();
 
 	console.log(menu);
 
-	return <div>Menu</div>;
+	return (
+		<ul>
+			{menu.map(pizza => (
+				<MenuItem pizza={pizza} key={pizza.id} />
+			))}
+		</ul>
+	);
 }
 
 export async function loader() {
-	const menu = await getMenu();
-	return menu;
+	return await getMenu();
 }
