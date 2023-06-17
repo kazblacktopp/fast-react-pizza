1655a388d1c8fe5278762aca4d92ac3f79fce688
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-17 13:50:19 +1000

    Add data loading to Menu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import AppLayout from './ui/AppLayout';
 import Home from './ui/Home';
-import Menu from './features/menu/Menu';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
@@ -13,7 +13,7 @@ function App() {
 			children: [
 				{ path: '/', element: <Home /> },
 				{ path: '/cart', element: <Cart /> },
-				{ path: '/menu', element: <Menu /> },
+				{ path: '/menu', element: <Menu />, loader: menuLoader },
 				{ path: '/order/new', element: <CreateOrder /> },
 				{ path: '/order/:orderID', element: <Order /> },
 			],
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,3 +1,15 @@
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+
 export default function Menu() {
+	const menu = useLoaderData();
+
+	console.log(menu);
+
 	return <div>Menu</div>;
+}
+
+export async function loader() {
+	const menu = await getMenu();
+	return menu;
 }
