008704ea7cb482444b144baf6726359fb65d7d65
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-17 19:18:52 +1000

    Create Spinner component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,16 @@
 import Header from './Header';
 import CartOverview from '../features/cart/CartOverview';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
+import Spinner from './Spinner';
 
 export default function AppLayout() {
+	const navigation = useNavigation();
+	const isLoading = navigation.state === 'loading';
+
 	return (
 		<div>
 			<Header />
+			{isLoading && <Spinner />}
 
 			<main>
 				<Outlet />
