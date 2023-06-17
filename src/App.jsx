cb4d34ef78df7a716fa07a96f39b4fcf398e42d2
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-17 12:11:44 +1000

    Set up react router to page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
+
 function App() {
-	return <div>Hello World!</div>;
+	const router = createBrowserRouter([
+		{ path: '/', element: <Home /> },
+		{ path: '/cart', element: <Cart /> },
+		{ path: '/menu', element: <Menu /> },
+		{ path: '/order/new', element: <CreateOrder /> },
+		{ path: '/order/:orderID', element: <Order /> },
+	]);
+
+	return <RouterProvider router={router} />;
 }
 
 export default App;
