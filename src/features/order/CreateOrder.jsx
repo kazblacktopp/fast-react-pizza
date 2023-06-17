73b46899ff6f55b5016df5532ad83341f6648441
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-17 12:10:56 +1000

    Create page components with boilerplate code

---
+++ src/features/order/CreateOrder.jsx
@@ -0,0 +1,3 @@
+export default function CreateOrder() {
+	return <div>Create Your Order</div>;
+}
---
+++ src/features/user/CreateUser.jsx
@@ -0,0 +1,3 @@
+export default function CreateUser() {
+	return <div>Welcome! Please add your details below:</div>;
+}
