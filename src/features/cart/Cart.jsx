641919251594d0f32759a2688a3caaf0038fa053
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-18 13:16:20 +1000

    Build Cart component

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -4,6 +4,13 @@ export default function Cart() {
 	return (
 		<div>
 			<Link to="/menu">&larr; Back to menu</Link>
+
+			<h2>Your cart, %NAME%</h2>
+
+			<div>
+				<Link to="/order/new">Order pizzas</Link>
+				<button>Clear cart</button>
+			</div>
 		</div>
 	);
 }
