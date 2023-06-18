3d3c77870837e1ecaf4da530831fc783a2bf9ccd
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-18 13:13:01 +1000

    Add grid styles

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,14 +8,15 @@ export default function AppLayout() {
 	const isLoading = navigation.state === 'loading';
 
 	return (
-		<div>
+		<div className="grid h-screen grid-rows-[auto_1fr_auto]">
 			<Header />
 			{isLoading && <Spinner />}
 
-			<main>
-				<Outlet />
-			</main>
-
+			<div className="overflow-scroll">
+				<main className="mx-auto max-w-3xl">
+					<Outlet />
+				</main>
+			</div>
 			<CartOverview />
 		</div>
 	);
