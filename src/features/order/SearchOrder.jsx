ed11318e04491dabc00ff55cdd4860a0e9bd20ce
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-18 20:39:21 +1000

    Increase search box width

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -23,7 +23,7 @@ export default function SearchOrder() {
 				value={query}
 				onChange={e => setQuery(e.target.value)}
 				autoComplete="off"
-				className="w-32 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:w-36 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
+				className="w-[8.5rem] rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:w-36 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
 			/>
 		</form>
 	);
