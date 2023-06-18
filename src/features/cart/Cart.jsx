bfe8b49a0df04ccf77d79d0539f5e92a6e34dee4
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-18 17:19:18 +1000

    Build Cart component with dummy data

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,10 +1,36 @@
-import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
+import CartItem from './CartItem';
+
+const fakeCart = [
+	{
+		pizzaId: 12,
+		name: 'Mediterranean',
+		quantity: 2,
+		unitPrice: 16,
+		totalPrice: 32,
+	},
+	{
+		pizzaId: 6,
+		name: 'Vegetale',
+		quantity: 1,
+		unitPrice: 13,
+		totalPrice: 13,
+	},
+	{
+		pizzaId: 11,
+		name: 'Spinach and Mushroom',
+		quantity: 1,
+		unitPrice: 15,
+		totalPrice: 15,
+	},
+];
 
 export default function Cart() {
+	const cart = fakeCart;
+
 	return (
-		<div>
+		<div className="px-4 py-3">
 			<LinkButton
 				to="/menu"
 				className="text-sm text-blue-500 hover:text-blue-600 hover:underline"
@@ -12,11 +38,20 @@ export default function Cart() {
 				&larr; Back to menu
 			</LinkButton>
 
-			<h2>Your cart, %NAME%</h2>
+			<h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-			<div>
-				<Button to="/order/new">Order pizzas</Button>
-				<button>Clear cart</button>
+			<ul className="divide-y divide-stone-200 border-b">
+				{cart.map(item => (
+					<CartItem item={item} key={item.pizzaId} />
+				))}
+			</ul>
+
+			<div className="mt-6 space-x-2">
+				<Button type="primary" to="/order/new">
+					Order pizzas
+				</Button>
+
+				<Button type="secondary">Clear cart</Button>
 			</div>
 		</div>
 	);
