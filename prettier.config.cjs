19d3184a7269b163c3bdb9b7f34159a5672db199
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-18 10:44:07 +1000

    Add tailwind prettier plugin

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+	plugins: [require('prettier-plugin-tailwindcss')],
+	singleQuote: true,
+	arrowParens: 'avoid',
+	trailingComma: 'all',
+	tabWidth: 4,
+	useTabs: true,
+	endOfLine: 'crlf',
+};
