53143ae41c59a80ed537f4d36f5b5b57d0f12df6
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-21 13:16:22 +1000

    Import DeleteItem component

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,16 +1,8 @@
-import { useDispatch } from 'react-redux';
-import Button from '../../ui/Button';
 import { formatCurrency } from '../../utilities/helpers';
-import { removeItem } from './cartSlice';
+import DeleteItem from './DeleteItem';
 
 export default function CartItem({ item }) {
 	const { pizzaId, name, quantity, totalPrice } = item;
-
-	const dispatch = useDispatch();
-
-	function handleDeleteItem() {
-		dispatch(removeItem(pizzaId));
-	}
 
 	return (
 		<li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -21,9 +13,7 @@ export default function CartItem({ item }) {
 				<p className="text-sm font-bold">
 					{formatCurrency(totalPrice)}
 				</p>
-				<Button onClick={handleDeleteItem} type="small">
-					Delete
-				</Button>
+				<DeleteItem pizzaId={pizzaId} />
 			</div>
 		</li>
 	);
