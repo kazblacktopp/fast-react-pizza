84d0709352d60d0db95b4940179674a3103d5056
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-21 14:39:29 +1000

    Create new round button type

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,13 +2,14 @@ import { Link } from 'react-router-dom';
 
 export default function Button({ children, disabled, to, type, onClick }) {
 	const base =
-		'inline-block rounded-full bg-yellow-400 px-4 text-sm font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-stone-300 disabled:text-opacity-50';
+		'inline-block rounded-full bg-yellow-400 text-sm font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-stone-300 disabled:text-opacity-50';
 
 	const styles = {
-		primary: base + ' py-3 md:px-6 md:py-4',
+		primary: base + ' px-4 py-3 md:px-6 md:py-4',
 		secondary:
 			'inline-block rounded-full border-2 border-stone-300 px-4 py-2.5 text-sm font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-3.5',
-		small: base + ' py-2 md:px-5 md:py-4 text-xs',
+		small: base + ' px-4 py-2 md:px-5 md:py-4 text-xs',
+		round: base + ' px-2.5 py-1.5 md:px-3 md:py-2 text-xs',
 	};
 
 	if (to)
