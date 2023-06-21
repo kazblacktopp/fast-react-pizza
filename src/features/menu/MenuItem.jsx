0d6ba8d097fcf249b9d5c24af5a1359201b79ee4
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-21 11:32:23 +1000

    Add access to Redux store and handleAddToCart function

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,24 @@
 import { formatCurrency } from '../../utilities/helpers';
 import Button from '../../ui/Button';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../cart/cartSlice';
 
 export default function MenuItem({ pizza }) {
-	const { name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+	const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+	const dispatch = useDispatch();
+
+	function handleAddToCart() {
+		const newItem = {
+			pizzaId: id,
+			name,
+			quantity: 1,
+			unitPrice,
+			totalPrice: unitPrice * 1,
+		};
+
+		dispatch(addItem(newItem));
+	}
 
 	return (
 		<li className="flex gap-4 py-2">
@@ -25,7 +41,11 @@ export default function MenuItem({ pizza }) {
 						</p>
 					)}
 
-					<Button type="small">Add to cart</Button>
+					{!soldOut && (
+						<Button type="small" onClick={handleAddToCart}>
+							Add to cart
+						</Button>
+					)}
 				</div>
 			</div>
 		</li>
