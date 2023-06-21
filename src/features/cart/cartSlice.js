ced0781ca76f8a10c4d0bf3b2f7516941be69077
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-21 11:31:40 +1000

    Add getCart, getTotalCartQuantity and getTotalCartPrice functions

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,16 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-	// Fake initialState
-	cart: [
-		{
-			pizzaId: 12,
-			name: 'Margherita',
-			quantity: 2,
-			unitPrice: 12,
-			totalPrice: 24,
-		},
-	],
+	cart: [],
 };
 
 const cartSlice = createSlice({
@@ -60,3 +51,11 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getCart = state => state.cart.cart;
+
+export const getTotalCartQuantity = state =>
+	state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = state =>
+	state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
